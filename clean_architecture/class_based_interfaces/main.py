1b9279eb44605ac6a27cf858514ea42111710e79
Ryan Rigato <ryanrigato@gmail.com>
2021-03-21 08:38:19 -0700

    invalid and valid input examples

--- clean_architecture/class_based_interfaces/main.py
+++ clean_architecture/class_based_interfaces/main.py
@@ -1,6 +1,8 @@
 from externals.request_object_api import ApiRequest
 from externals.request_network_failure import NetworkFailure
+
 if __name__ == "__main__":
     valid_input = ApiRequest()
-    invalid_input = NetworkFailure()
-    import pdb; pdb.set_trace()
+    print(NetworkFailure(error_message="Network timeout").get_error_message())
+    #invalid input example
+    #print(NetworkFailure(error_message=12).get_error_message())
--- clean_architecture/class_based_interfaces/usecase/request_object_contract.py
+++ clean_architecture/class_based_interfaces/usecase/request_object_contract.py
@@ -18,13 +18,16 @@ class InvalidRequest(ABC):
         """Returns False"""
         return(False)
 
-    @property 
-    def error_message(self):
-        return(self._error_message)
+    def __init__(self, error_message):
+        self._set_error_message(error_message=error_message)
 
-    @error_message.setter     
-    def error_message(self, error_message):
+    def get_error_message(self):
+        return(self._error_message)
+     
+    def _set_error_message(self, error_message):
+        """Private to ensure error_message is immutable, error_message is set once for the
+        value object and never changed
         """
-        """
-        assert type(error_message) == str, "InvalidRequest - error_message must be type str" 
+        if type(error_message) != str:
+            raise TypeError("InvalidRequest - error_message must be type str")
         self._error_message = error_message
