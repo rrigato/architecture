e2f56b71e134864cad16c8d4f20f5b8e54baa955
Ryan Rigato <ryanrigato@gmail.com>
2021-03-21 11:09:30 -0700

    spacing

--- clean_architecture/class_based_interfaces/main.py
+++ clean_architecture/class_based_interfaces/main.py
@@ -4,8 +4,10 @@ from usecase.business_rules import customer_count
 
 if __name__ == "__main__":
     valid_input = CustomerInfo().input_validation({"entity_name": "HighLevelObject"})
+    
     print(valid_input.get_filters())
     print(NetworkFailure(error_message="Network timeout").get_error_message())
+
     if bool(valid_input) == True:
         print(customer_count()) 
     #invalid input example
