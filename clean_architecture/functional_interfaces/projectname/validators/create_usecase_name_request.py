a92a068a8815ab5acc80bd7c9ecf2c7eba1e31a0
Ryan <ryanrigato@gmail.com>
2021-04-18 09:40:46 -0500

    stubbing out create function responsibilities

--- clean_architecture/functional_interfaces/projectname/validators/create_usecase_name_request.py
+++ clean_architecture/functional_interfaces/projectname/validators/create_usecase_name_request.py
@@ -1,9 +1,30 @@
-def validate_usecase_name(arg1, arg2):
+from projectname.validators.check_input import arg1_range
+from projectname.validators.request_objects import InvalidRequest
+from projectname.validators.request_objects import ValidRequest
+
+def validate_usecase_name(arg1):
     """
+        Parameters
+        ----------
+        arg1 : int
+            in the interval (0,100]
+            
+
+        Returns
+        -------
+        request_object : ValidRequest or InvalidRequest
+        ValidRequest if all preconditions to invoke usecase_name are met,
+        otherwise an InvalidRequst. If ValidRequest, usecase_name
+        has all input neceassary to invoke its interface and the only 
+        thing that can result in a failure is if the usecase has an unexpected
+        error
     """
     '''
         Performs the following:
             - data type checking of arguements
-            - 
+            - Prequisites for usecase design by contract
+            - collects any business logic specific input domain
+            validation
     '''
-    pass
+    if bool(arg1_range(arg1=arg1)) == False:
+        return()
