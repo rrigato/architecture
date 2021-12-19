09025bb1b9b4a22014143e9b19317c5a41dea648
Ryan Rigato <ryanrigato@gmail.com>
2021-12-19 14:18:34 -0800

    unhappy path test case

--- clean_architecture/functional_interfaces/tests/test_entry/test_request_objects.py
+++ clean_architecture/functional_interfaces/tests/test_entry/test_request_objects.py
@@ -2,7 +2,7 @@ from copy import deepcopy
 import unittest
 
 class TestRequestObjects(unittest.TestCase):
-    def test_burn_status(self):
+    def test_valid_request(self):
         """Happy Path ValidRequest for different request filters"""
         from projectname.entry.request_objects import ValidRequest
 
@@ -20,6 +20,25 @@ class TestRequestObjects(unittest.TestCase):
 
                 self.assertEqual(mock_request_filter, mock_request_object.request_filters)
                 self.assertTrue(bool(mock_request_object))
+
+    def test_valid_request_bad_input(self):
+        """Unhappy Path ValidRequest invalid datatype for request_filters"""
+        from projectname.entry.request_objects import ValidRequest
+
+
+        mock_request_filters = [
+            set(["a", "b"]),
+            "arg1", 
+            1, 
+            2.0,
+            (1, 2, 3)
+        ]
+        for mock_request_filter in mock_request_filters:
+            with self.subTest(mock_request_filter=mock_request_filter):
+
+                with self.assertRaises(TypeError):
+                    mock_request_object = ValidRequest(request_filters=deepcopy(mock_request_filter))
+
 
 
 
