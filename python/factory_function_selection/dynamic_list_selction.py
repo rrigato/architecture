4bfea8f32779f65d6186ee76e42b92029a9e384b
Ryan Rigato <ryanrigato@gmail.com>
2021-12-21 12:29:19 -0800

    list based routing

--- python/factory_function_selection/dynamic_list_selction.py
+++ python/factory_function_selection/dynamic_list_selction.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 def factory_function1():
     """"""
     print("factory_function1")
@@ -13,8 +15,16 @@ def factory_function_default():
     print("factory_function_default")
 
 
-router = {
-    "one": factory_function1,
-    "two": factory_function2,
-    "default": factory_function_default
-}
+def factory_router():
+    """Routes to function based on dict key selected"""
+    complete_routing_dict = {}
+    for int_index in range(100, 150):
+        complete_routing_dict[int_index] = factory_function1 
+
+    for int_index in range(150, 200):
+        complete_routing_dict[int_index] = factory_function2 
+    return(
+        deepcopy(
+            complete_routing_dict
+        )
+    )
---
+++ python/factory_function_selection/dynamic_routing.py
@@ -0,0 +1,4 @@
+from factory_function_selection.dynamic_list_selction import factory_router
+
+factory_router()[100]()
+factory_router()[199]()
