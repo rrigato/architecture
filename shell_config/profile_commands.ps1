563c809ecfcee9403e521fdd8250ed138c1f8166
Ryan <ryanrigato@gmail.com>
2022-03-21 10:07:43 -0500

    place shell_config in root of project

---
+++ shell_config/profile_commands.ps1
@@ -0,0 +1,6 @@
+##################################
+#Documentation on where to install:
+#https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-5.1#the-profile-files
+#
+#
+#################################
--- shell_config/setup_alias.sh
+++ shell_config/setup_alias.sh
@@ -5,7 +5,7 @@
 #use alias command to list all alias that are present
 ################################
 
-while IFS=',' read alias_name alias_value
+while IFS=',' read -r alias_name alias_value
 do
     alias $alias_name="$alias_value"
-done < <(tail -n +2 ~/shell_config/alias_commands.csv)
+done < <(tail -n +2 ./shell_config/alias_commands.csv)
