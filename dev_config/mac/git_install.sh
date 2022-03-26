912ec3a146001c9594136ed265dcd17bda9a9fb2
rrigato <ryanrigato@gmail.com>
2022-03-26 14:59:44 -0500

    ssh key setup

--- dev_config/mac/git_install.sh
+++ dev_config/mac/git_install.sh
@@ -3,3 +3,14 @@ brew install git
 git config --global user.email <email>
 
 git config --global user.name <user_name>
+
+
+# check ssh keys
+ls -al ~/.ssh
+
+#creating a new ssh key
+ssh-keygen -t ed25519 -C "<email_address>"
+
+#copy public key to clipboard
+pbcopy < ~/.ssh/id_ed25519.pub
+# go to remote to to paste your public key
