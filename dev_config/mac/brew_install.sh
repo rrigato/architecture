b346986111a8574b67b6eff3bfd223f51a040076
rrigato <ryanrigato@gmail.com>
2022-03-27 09:55:51 -0500

    bash_profile setup for mac

--- dev_config/mac/brew_install.sh
+++ dev_config/mac/brew_install.sh
@@ -1,7 +1,2 @@
 #install home-brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-#add brew to bash_profile
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/ryan/.bash_profile
-
-eval "$(/opt/homebrew/bin/brew shellenv)"
