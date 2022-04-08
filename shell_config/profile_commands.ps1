d04824fece2d0fe454e94917cd7e50ac385bcad0
Ryan Rigato <ryanrigato@gmail.com>
2022-04-08 15:27:39 -0700

    first pass at dynamic function creation

--- shell_config/profile_commands.ps1
+++ shell_config/profile_commands.ps1
@@ -4,3 +4,22 @@
 #
 #
 #################################
+
+$alias_list = Import-Csv "shell_config\alias_commands.csv" -Header "alias_name", "alias_value" 
+
+
+foreach($alias_row in $alias_list){
+    Function global:: "$alias_row.alias_name" {
+        $alias_row.alias_value
+    }
+
+}
+
+for ($counter = 1 ; $counter -le 10 ; $counter++){  
+    echo "$alias_list[$counter].alias_name";
+    
+    Function "$alias_list[$counter].alias_name"{
+        $alias_list[$counter].alias_value
+
+    }
+}
